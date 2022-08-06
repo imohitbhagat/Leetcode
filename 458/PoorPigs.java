18dbe9e906c9dfdd33a25e0eefe420229c8303f4
MOHIT BHAGAT <mohitbhagat58@gmail.com>
2022-08-06 20:48:39 +0530

    Create PoorPigs.java

---
+++ 458/PoorPigs.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        
+        return (int) Math.ceil(Math.log(buckets) / Math.log((minutesToTest/minutesToDie) + 1));
+    }
+}
